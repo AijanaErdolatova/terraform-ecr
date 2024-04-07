bfdf3952d7a2741723c2d2fbcf8ffe2dd3e9d3a8
aijana <aijana.erdo@gmail.com>
2024-04-07 16:52:09 -0500

    adds

--- main.tf
+++ main.tf
@@ -1,10 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "ecr-tf-state-aijana"
+    //dynamodb_table = "state_lock"
+    key    = "tf-ecr"
+    region = "us-east-1"
+  }
+}
+
 
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_ecr_repository" "my_ecr_repo" {
-  name                 = "my-ecr-repo"
+  name                 = "my-ecr-repo12"
   image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
